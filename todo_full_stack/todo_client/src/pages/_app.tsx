e73116e843f23faa0bbddc0a8637b3e4e3d7549b
now <now@marketon.shop>
2023-04-11 19:40:43 +0900

    init cli

---
+++ todo_full_stack/todo_client/src/pages/_app.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      login
+      <input type="text" />
+      <input type="password" />
+      <button>login</button>
+    </main>
+  );
+}
---
+++ todo_full_stack/todo_client/src/pages/loggedIn.tsx
@@ -0,0 +1,10 @@
+export default function Index() {
+  return (
+    <main>
+      login
+      <input type="text" />
+      <input type="password" />
+      <button>login</button>
+    </main>
+  );
+}
